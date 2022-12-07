dea99eeaedda199106975c8dd6fea2763c3e3565
211410666 <211410666@o365.tku.edu.tw>
2022-12-07 23:32:22 +0800

    ### make logo to Portfolio & show Portfolio_img by tiles

--- md/project.md
+++ md/project.md
@@ -14,3 +14,7 @@
 
 ![](4-1.png)
 ![](4-2.png)
+
+### make logo to Portfolio & show Portfolio_img by tiles
+
+![](5.png)
---
+++ portfolio_66/js/portfolio.js
@@ -0,0 +1,9 @@
+function chage(index) {
+    const header = document.getElementById('header');
+    console.log('header', header);
+    switch (index) {
+        case 1:
+            header.textContent="C語言-河內塔"
+            break;
+    }
+}
