f23c28758e048fa6faef141dde3f196e1aef3c00
211410666 <211410666@o365.tku.edu.tw>
2022-12-10 19:09:15 +0800

    clear

--- portfolio_66/js/portfolio.js
+++
@@ -1,9 +0,0 @@
-function chage(index) {
-    const header = document.getElementById('header');
-    console.log('header', header);
-    switch (index) {
-        case 1:
-            header.textContent="C語言-河內塔"
-            break;
-    }
-}
