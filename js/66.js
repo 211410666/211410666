fe2314c97ff2d7384a166803a85529bd9c63b11d
211410666 <211410666@o365.tku.edu.tw>
2022-12-11 18:50:31 +0800

    ### Finish sidebar transition and fix the position on condition (max-width < 850)

--- js/66.js
+++ js/66.js
@@ -1,4 +1,24 @@
 const nav = document.querySelector('#nav');
+const sidebar = document.getElementById('sidebar');
+const closeBtn = document.getElementById('close-btn');
+const navBtn = document.getElementById('nav-btn');
+
+
+
+console.log('navBtn', navBtn);
+console.log('sidebar', sidebar);
+
+
+window.addEventListener('resize', function () {
+  if (window.outerWidth > 850) {
+    navBtn.style.display = "none";
+    closeBtn.style.display = "none";
+    sidebar.classList.remove('show-sidebar');
+  }
+  else {
+    navBtn.style.display = "block";
+  }
+});
 
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > 20) {
@@ -6,4 +26,18 @@ window.addEventListener('scroll', function () {
     } else {
       nav.classList.remove('nav-fix');
     }
-  });
+});
+  
+navBtn.addEventListener('click', function () {
+  sidebar.classList.add('show-sidebar');
+  closeBtn.style.display = "block";
+  navBtn.style.display = "none";
+    
+});
+
+closeBtn.addEventListener('click', function () {
+  sidebar.classList.remove('show-sidebar');
+  closeBtn.style.display = "none";
+  navBtn.style.display = "block";
+    
+});
