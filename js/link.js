4a3bcfab8bdcc19e93cae355a63de7ceb51c61fd
211410666 <211410666@o365.tku.edu.tw>
2022-12-17 22:51:34 +0800

    temp

--- js/link.js
+++ js/link.js
@@ -3,8 +3,19 @@ function change(index) {
     
     switch (index) {
         case 1:
-            console.log(p);
             p.innerHTML=`<iframe src="./portfolio/c_1.html" 
+            width="100%" height="100%" frameborder="0" border="0" cellspacing="0" />`
+            break;
+    }
+    switch (index) {
+        case 2:
+            p.innerHTML=`<iframe src="./portfolio/c_2.html" 
+            width="100%" height="100%" frameborder="0" border="0" cellspacing="0" />`
+            break;
+    }
+    switch (index) {
+        case 3:
+            p.innerHTML=`<iframe src="./portfolio/c_3.html" 
             width="100%" height="100%" frameborder="0" border="0" cellspacing="0" />`
             break;
     }
