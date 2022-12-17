39de9a291370dbbdeb506d78cfc716d3119d7e0b
211410666 <211410666@o365.tku.edu.tw>
2022-12-17 20:55:45 +0800

    ### design contect in portfolio and use iframe without border link to another one

--- js/link.js
+++ js/link.js
@@ -1,11 +1,11 @@
 function change(index) {
-    const p = document.querySelector('.index');
+    const p = document.querySelector('.portfolio_c-center');
     
     switch (index) {
-        case 'portfolio_c':
-            console.log('123');
-            p.innerHTML=`<iframe src="./portfolio/portfolio_c.html" 
-            width="100%" height="100%" />`
+        case 1:
+            console.log(p);
+            p.innerHTML=`<iframe src="./portfolio/c_1.html" 
+            width="100%" height="100%" frameborder="0" border="0" cellspacing="0" />`
             break;
     }
 }
